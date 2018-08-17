f16d3ee49e96abcd9fd25d9d27b9635440630b97
Vladimir Kireyko <v.kireyko@amgrade.com>
2018-08-17 10:22:26 +0300

    Add Docs to interfaces.

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,10 +5,35 @@ import {Laroute} from './laroute';
 
 declare module "vue/types/vue" {
   export interface Vue {
+    /** Vue instance that used as event bus. */
     $vbus: _Vue;
+
+    /**
+     * Laravel Echo
+     *
+     * @url https://laravel.com/docs/5.6/broadcasting#installing-laravel-echo
+     */
     $echo: _Echo;
+
+    /**
+     * SweetAlert
+     *
+     * @url https://github.com/t4t5/sweetalert
+     */
     $swal: SweetAlert;
+
+    /**
+     * Toastr
+     *
+     * @url https://github.com/CodeSeven/toastr
+     */
     $toastr: Toastr;
+
+    /**
+     * Laravel routes on JS.
+     *
+     * @url https://github.com/aaronlord/laroute
+     */
     $laroute: Laroute;
   }
 }
--- types/laroute.d.ts
+++ types/laroute.d.ts
@@ -1,8 +1,59 @@
+/**
+ * Laravel routes on JS.
+ *
+ * @url https://github.com/aaronlord/laroute#javascript-documentation
+ */
 export interface Laroute {
+  /**
+   * Generate a URL for a given controller action.
+   *
+   * @param action     - The action to route to.
+   * @param parameters - Optional. key:value object literal of route parameters.
+   */
   action: (action: string, parameters?: object) => string;
+
+  /**
+   * Generate a URL for a given named route.
+   *
+   * @param name       - The name of the route to route to.
+   * @param parameters - Optional. key:value object literal of route parameters.
+   */
   route: (name: string, parameters?: object) => string;
+
+  /**
+   * Generate a fully qualified URL to the given path.
+   *
+   * @param name       - The name of the route to route to.
+   * @param parameters - Optional. value array of route parameters.
+   */
   url: (name: string, parameters?: Array<string>) => string;
+
+  /**
+   * Generate a html link to the given url.
+   *
+   * @param url        - A relative url.
+   * @param title      - Optional. The anchor text to display.
+   * @param attributes - Optional. key:value object literal of additional html attributes.
+   */
   link_to: (url: string, title?: string, attributes?: object) => string;
+
+  /**
+   * Generate a html link to the given route.
+   *
+   * @param name       - The name of the route to route to.
+   * @param title      - Optional. The anchor text to display.
+   * @param parameters - Optional. key:value object literal of route parameters.
+   * @param attributes - Optional. key:value object literal of additional html attributes.
+   */
   link_to_route: (name: string, title?: string, parameters?: object, attributes?: object) => string;
+
+  /**
+   * Generate a html link to the given action.
+   *
+   * @param action     - The action to route to.
+   * @param title      - Optional. The anchor text to display
+   * @param parameters - Optional. key:value object literal of route parameters.
+   * @param attributes - Optional. key:value object literal of additional html attributes.
+   */
   link_to_action: (action: string, title?: string, parameters?: object, attributes?: object) => string;
 }
